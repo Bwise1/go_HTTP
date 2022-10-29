94ab2ae658a12c6f1493414d12268585d19fea2d
benjamin oguntoye <benjys@Benjys.local>
2022-10-29 09:37:27 +0300

    http server in golang

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/hng_task1
+
+go 1.19
+
+require github.com/gorilla/mux v1.8.0
---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func enableCors(w *http.ResponseWriter) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+}
+
+type details struct {
+	SlackUsername string `json:"slackUsername"`
+	Backend bool `json:"backend"`
+	Age int `json:"age"`
+	Bio string `json:"bio"`
+}
+
+func task(w http.ResponseWriter, req *http.Request) {
+	data := &details{SlackUsername:"Benjys", Backend:true, Age: 27, Bio: "I don't want peace, I am a fight ..."}
+	enableCors(&w)
+
+	w.Header().Set("Content-Type", "application/json")
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		fmt.Printf("could not marshal json: %s\n", err)
+		return
+	}
+	w.Write(jsonData)
+
+
+}
+
+
+func main() {
+
+	r := mux.NewRouter()
+
+
+
+	r.HandleFunc("/", task).Methods("GET")
+
+	fmt.Println("Server started at port 8090")
+	log.Fatal(http.ListenAndServe(":8090", r))
+}
